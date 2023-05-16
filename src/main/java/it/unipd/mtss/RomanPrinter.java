02c775a2698d01076ac7b0a606dbfc9fa09b6b02
Davide <davide.picello2002@gmail.com>
2023-05-16 11:59:01 +0200

    Configurazione iniziale

---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,12 @@
+package it.unipd.mtss;
+
+public class RomanPrinter {
+  public static String print(int num){
+    return printAsciiArt(IntegerToRoman.convert(num));
+  }
+
+  public static String printAsciiArt(String romanNumber){
+
+    return null;
+  }
+}
